d5c46f021f8718615bcc85e83dff8d92a3c7e4f8
Kyle Krull <kkrull@8thlight.com>
2023-08-01 18:14:06 -0500

    Read name of first chunk

--- midi-parse/scratch.rb
+++ midi-parse/scratch.rb
@@ -1,8 +1,15 @@
 filename = 'data/drum-library-ezdrummer2.mid'
+
+def read_chunk(f)
+  type_name = f.read 4
+  chunk_bytes = f.read 4
+  return type_name, chunk_bytes.unpack1('N')
+end
+
 f = File.open filename, 'rb'
 begin
-  contents = f.read
-  puts contents[0,4]
+  type_name, chunk_bytes = read_chunk f
+  puts "%s: %s bytes" % [type_name, chunk_bytes]
 ensure
   f.close
 end
