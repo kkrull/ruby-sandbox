fb2ff735c598596930bf6dd7be6310537e467bf8
Kyle Krull <kkrull@8thlight.com>
2023-08-01 18:19:04 -0500

    Read another chunk header, unrolled

--- midi-parse/scratch.rb
+++ midi-parse/scratch.rb
@@ -1,6 +1,6 @@
 filename = 'data/drum-library-ezdrummer2.mid'
 
-def read_chunk(f)
+def read_chunk_header(f)
   type_name = f.read 4
   chunk_bytes = f.read 4
   return type_name, chunk_bytes.unpack1('N')
@@ -8,7 +8,11 @@ end
 
 f = File.open filename, 'rb'
 begin
-  type_name, chunk_bytes = read_chunk f
+  type_name, chunk_bytes = read_chunk_header f
+  puts "%s: %s bytes" % [type_name, chunk_bytes]
+
+  f.read chunk_bytes
+  type_name, chunk_bytes = read_chunk_header f
   puts "%s: %s bytes" % [type_name, chunk_bytes]
 ensure
   f.close
