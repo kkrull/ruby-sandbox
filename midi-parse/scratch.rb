059b58e2d3ddc0cf08fa7fcfec1ff25130fb622f
Kyle Krull <kkrull@8thlight.com>
2023-08-08 08:54:15 -0500

    Start decoding the header

--- midi-parse/scratch.rb
+++ midi-parse/scratch.rb
@@ -6,13 +6,25 @@ def read_chunk_header(f)
   return type_name, chunk_bytes.unpack1('N')
 end
 
+def inspect_chunk(type_name, chunk_bytes, f)
+  if 'MThd'.eql? type_name
+    puts 'Header chunk'
+    file_format = f.read 2
+    puts "File format: %d" % [file_format.unpack1('n')]
+    f.read(chunk_bytes - 2)
+  else 
+    f.read chunk_bytes
+  end
+end
+
 f = File.open filename, 'rb'
 begin
   while not f.eof?
     type_name, chunk_bytes = read_chunk_header f
+    puts ""
     puts "%s: %s bytes" % [type_name, chunk_bytes]
 
-    f.read chunk_bytes
+    inspect_chunk type_name, chunk_bytes, f
   end
 ensure
   f.close
