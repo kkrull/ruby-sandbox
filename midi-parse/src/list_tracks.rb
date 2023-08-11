bc203914587f2e2ea92fc52758ee1c276beb6251
Kyle Krull <kkrull@8thlight.com>
2023-08-11 09:03:42 -0500

    Read the header chunk

--- midi-parse/src/list_tracks.rb
+++ midi-parse/src/list_tracks.rb
@@ -5,11 +5,29 @@ end
 filename = ARGV[0]
 file = File.open(filename, 'rb')
 
-begin
-  chunk_type = file.read 4
-  chunk_length = file.read(4).unpack1("N")
+class MIDIChunk
+  def self.start_read(file)
+    chunk_type = file.read 4
+    chunk_length = file.read(4).unpack1("N")
+    MIDIChunk.new(chunk_type, chunk_length)
+  end
 
-  puts "%s: %d bytes" % [chunk_type, chunk_length]
+  attr_reader :length, :type
+
+  def initialize(type, length)
+    @type = type
+    @length = length
+  end
+end
+
+begin
+  chunk = MIDIChunk.start_read file
+  puts "%s: %d bytes" % [chunk.type, chunk.length]
+
+  file.read(2) #format
+  num_tracks = file.read(2).unpack1("n")
+  file.read(2) #division
+  puts "Tracks: %d" % num_tracks
 ensure
   file.close
 end
