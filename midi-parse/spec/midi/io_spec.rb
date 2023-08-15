5c3a0f100de1803e0fbc0ed106929e765b0848b5
Kyle Krull <kkrull@8thlight.com>
2023-08-15 18:25:46 -0500

    Implement base case - no data

--- midi-parse/spec/midi/io_spec.rb
+++ midi-parse/spec/midi/io_spec.rb
@@ -1,16 +1,29 @@
 require "rspec"
 
+require "stringio"
+
 module MIDI
   module_function
-  def read_variable_length_quantity()
+
+  def read_variable_length_quantity(str_io)
+    return [0, nil] # if str_io.eof?
+    #byte = str_io.readbyte
   end
 end
 
 RSpec.describe MIDI do
   describe "::read_variable_length_quantity" do
-    it "exists" do
-      expect(MIDI).to respond_to(:read_variable_length_quantity)
+    context "given a stream already at EOF" do
+      let(:data) { StringIO.new }
+      it "reads 0 bytes" do
+        MIDI.read_variable_length_quantity data
+        expect(data).not_to receive(:getbyte)
+      end
+
+      it "returns 0 bytes read, nil data" do
+        returned = MIDI.read_variable_length_quantity data
+        expect(returned).to eql([0, nil])
+      end
     end
   end
 end
-
