3acad3ffd969ec526ca6ee4d45597629a0c9c35f
Kyle Krull <kkrull@8thlight.com>
2023-08-15 10:05:13 -0500

    Sort configuration

--- midi-parse/spec/spec_helper.rb
+++ midi-parse/spec/spec_helper.rb
@@ -15,29 +15,29 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   begin
-    # Enable focusing via :focus metadata, fit, fdescribe, fcontext
-    config.filter_run_when_matching :focus
-
-    # Enable --only-failures and --next-failure by storing state
-    config.example_status_persistence_file_path = "spec/cli-state-examples.txt"
-
     # Disallow monkey-patch syntax
     # https://rspec.info/features/3-12/rspec-core/configuration/zero-monkey-patching-mode/
     config.disable_monkey_patching!
 
-    # Enable warnings
-    config.warnings = true
+    # Enable --only-failures and --next-failure by storing state
+    config.example_status_persistence_file_path = "spec/cli-state-examples.txt"
 
     # Verbose output when focused on one spec file, unless formatter already given
     if config.files_to_run.one?
       config.default_formatter = "doc"
     end
 
-    # Print the 10 slowest examples
-    config.profile_examples = 10
+    # Enable focusing via :focus metadata, fit, fdescribe, fcontext
+    config.filter_run_when_matching :focus
 
     # Order randomly, unless using --seed to order deterministically for Heisenbugs)
     config.order = :random
     Kernel.srand config.seed
+
+    # Print the 10 slowest examples
+    config.profile_examples = 10
+
+    # Enable warnings
+    config.warnings = true
   end
 end
