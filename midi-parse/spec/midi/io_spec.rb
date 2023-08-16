ee2aca1134e46aff247b07fec8872dbe98c132f1
Kyle Krull <kkrull@8thlight.com>
2023-08-16 10:04:36 -0500

    Rename variables and add return type

--- midi-parse/spec/midi/io_spec.rb
+++ midi-parse/spec/midi/io_spec.rb
@@ -6,15 +6,16 @@ require_relative "../../src/midi/io" #Absolute import?
 RSpec.describe MIDI do
   describe "::read_variable_length_quantity" do
     context "given a stream already at EOF" do
-      let(:data) { StringIO.new }
+      let(:io_stream) { StringIO.new }
       it "reads 0 bytes" do
-        expect(data).not_to receive(:getbyte)
-        MIDI.read_variable_length_quantity data
+        expect(io_stream).not_to receive(:getbyte)
+        MIDI.read_variable_length_quantity io_stream
       end
 
       it "returns 0 bytes read, nil data" do
-        returned = MIDI.read_variable_length_quantity data
-        expect(returned).to eql([0, nil])
+        returned = MIDI.read_variable_length_quantity io_stream
+        expect(returned.num_bytes_read).to eql(0)
+        expect(returned.data).to be_nil
       end
     end
 
--- midi-parse/src/midi/io.rb
+++ midi-parse/src/midi/io.rb
@@ -1,9 +1,19 @@
 module MIDI
   module_function
 
-  def read_variable_length_quantity(str_io)
-    return [0, nil] # if str_io.eof?
+  def read_variable_length_quantity(io_stream)
+    return Quantity.new(0, nil)
+    #return [0, nil] # if str_io.eof?
     #byte = str_io.readbyte
+  end
+
+  class Quantity
+    attr_reader :num_bytes_read, :data
+
+    def initialize(num_bytes_read, data)
+      @num_bytes_read = num_bytes_read
+      @data = data
+    end
   end
 end
 
